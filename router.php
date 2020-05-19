8082c7108e6f0f101bb6771c254b96085506cff6
alexisleonel1 <alexisleogalvan12@gmail.com>
2020-05-19 13:23:39 -0300

    se creo el router

---
+++ router.php
@@ -0,0 +1,21 @@
+<?php
+    require_once 'controllers/cars.controller.php';
+    require_once 'controllers/admin.controller.php';
+
+    // definimos la base url de forma dinamica
+    define('BASE_URL', '//'.$_SERVER['SERVER_NAME'] . ':' . $_SERVER['SERVER_PORT'] . dirname($_SERVER['PHP_SELF']).'/');
+
+    // define una acción por defecto
+    if (empty($_GET['action'])) {
+        $_GET['action'] = 'inicio';
+    } 
+
+    // toma la acción que viene del usuario y parsea los parámetros
+    $accion = $_GET['action']; 
+    $parametros = explode('/', $accion);
+    //var_dump($parametros); die; // like console.log();
+
+    // decide que camino tomar según TABLA DE RUTEO
+    switch ($parametros[0]) {
+
+    }
