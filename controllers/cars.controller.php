0451edc35611a4f0a7b4d9d123d1df93b901e840
Nacho Almeida <nachoalmeida77@gmail.com>
2020-05-20 19:39:00 -0300

    Se crea cars.controller y la funcion showCars dentro del mismo

---
+++ controllers/cars.controller.php
@@ -0,0 +1,20 @@
+<?php
+
+class CarsController{
+
+    private $carsModel;
+    private $carsView;
+
+    public function __construct(){
+
+        $this->carsModel = new CarsModel();        
+        $this->carsView = new CarsView();
+    }
+
+    public function showCars(){
+        //Traigo todas las publicaciones del modelo
+        $cars=$this->carsModel->getAllCars();
+        //Muestro todas las publicaciones en la vista
+        $this->carsView->show_cars($cars);
+    }
+}
